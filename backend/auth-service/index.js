dcfc8ee8e2ef0e99185b8568c476d9ed275f2706
vaishnavi310804 <vaishnavikumari3108@gmail.com>
2026-07-25 22:48:41 +0530

    update

--- backend/auth-service/index.js
+++ backend/auth-service/index.js
@@ -19,6 +19,6 @@ app.get("/",(req,res)=>{
   }); 
 
 const PORT = process.env.PORT || 5000;
-app.listen(5000, "0.0.0.0", () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log("Server running on port 5000");
 });
