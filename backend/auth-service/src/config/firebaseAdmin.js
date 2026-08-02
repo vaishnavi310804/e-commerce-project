ac7f45ab9c9d57920f6b50239dc4a3bec55d96bc
vaishnavi310804 <vaishnavikumari3108@gmail.com>
2026-08-03 02:02:44 +0530

    a

--- backend/auth-service/src/config/firebaseAdmin.js
+++ backend/auth-service/src/config/firebaseAdmin.js
@@ -1,12 +1,14 @@
 import { initializeApp, cert, getApps } from "firebase-admin/app";
 import { getMessaging } from "firebase-admin/messaging";
-import serviceAccount from "../../credentials/shopease-651d2-firebase-adminsdk-fbsvc-8afaca108e.json" with { type: "json" };
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 if (!getApps().length) {
   initializeApp({
     credential: cert(serviceAccount),
   });
 }
+
 const messaging = getMessaging();
 
 export default messaging;
